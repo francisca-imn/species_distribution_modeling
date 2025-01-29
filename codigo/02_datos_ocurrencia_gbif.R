c081fcbce91817d315e7a2f355870899043214cf
Fran <fmnarbona@uc.cl>
2025-01-29 12:03:40 -0300

    arreglue el codigo de obtención de ocurrencias (GBIF)

--- codigo/02_datos_ocurrencia_gbif.R
+++ codigo/02_datos_ocurrencia_gbif.R
@@ -7,47 +7,20 @@ library(rnaturalearthdata)
 library(sf)
 library(rJava)
 
-# verificando la existencia de la especie en GBIF -------------------------
+# Obteniendo data de GBIF -------------------------------------------------
 especie_info <- name_backbone(name = "Lycalopex culpaeus")  #cambiar el nombre por el de la especie de interés
-print(especie_info)
-taxon_key <- especie_info$usageKey
 
-especie_data <- occ_search(taxonKey = taxon_key,
-                          country = "CL",
-                          hasCoordinate = TRUE,
-                          limit = 5000)  #la cantidad de datos puede variar
-
+especie_data <- occ_search(taxonKey = especie_info$usageKey,                         
+                           country = "CL",                    #solo datos de Chile
+                           hasCoordinate = TRUE,              #solo registros con coordenadas
+                           hasGeospatialIssue = FALSE,
+                           limit = 5000)        #sin problemas espaciales
+                        
 data_ocurrencias <- especie_data$data
 
 # en base a observaciones -------------------------------------------------
 data_ocurrencias_filtered <- data_ocurrencias %>%
   filter(basisOfRecord %in% c("HUMAN_OBSERVATION", "MACHINE_OBSERVATION"))
-
-# mmm veamos issues mejor -------------------------------------------------
-unique(data_ocurrencias$issues)
-
-data_ocurrencias_filtered <- data_ocurrencias %>%  #OJO, ESTO ELIMINA TODOS LOS DATOS
-  filter(issues == "")  # Solo registros sin problemas reportados
-
-
-# mmmmmm aber, viendo instituciones de origen -----------------------------
-unique(data_ocurrencias$publishingOrgKey)
-
-get_org_name <- function(org_key) {              # Función para obtener el nombre de la organización por su publishingOrgKey
-  url <- paste0("https://api.gbif.org/v1/organization/", org_key)
-  response <- fromJSON(content(GET(url), "text"), flatten = TRUE)
-  return(response$title)
-}
-
-# Aplicar la función a los códigos únicos en tus datos
-org_names <- unique(data_ocurrencias$publishingOrgKey)
-org_info <- sapply(org_names, get_org_name)
-
-print(org_info) # Ver los resultados y analizar si alguno no corresponde
-
-#28eb1a3f-1c15-4a95-931a-4af90ecb574d      1f00d75c-f6fc-4224-a595-975e82d7689c      e2e717bf-551a-4917-bdc9-4fa0f342c530 
-#         "iNaturalist.org"              "Xeno-canto Foundation for Nature Sounds"        "Cornell Lab of Ornithology"
-
 
 # Eliminar datos de iNaturalist.org ---------------------------------------
 data_ocurrencias_filtered <- data_ocurrencias_filtered %>%
@@ -56,10 +29,6 @@ data_ocurrencias_filtered <- data_ocurrencias_filtered %>%
 # Precisión espacial adecuada ---------------------------------------------
 data_ocurrencias_filtered <- data_ocurrencias_filtered %>%
   filter(coordinateUncertaintyInMeters <= 1000 | is.na(coordinateUncertaintyInMeters))  # Incertidumbre < 1 km
-
-# Fecha (actuales) --------------------------------------------------------
-data_ocurrencias_filtered <- data_ocurrencias_filtered %>%  #esto no porque el modelo actual abarca hartos años
-  filter(year >= 2023)  # Solo registros de los últimos 2 años
 
 # cautiverio o zoo --------------------------------------------------------
 data_ocurrencias_filtered <- data_ocurrencias_filtered %>%
@@ -72,3 +41,40 @@ data_ocurrencias_filtered <- data_ocurrencias_filtered %>%
 # evitar duplicados -------------------------------------------------------
 data_ocurrencias_filtered <- data_ocurrencias_filtered %>%
   distinct(decimalLongitude, decimalLatitude, year, .keep_all = TRUE)
+
+# 📊 Resultados y visualización -------------------------------------------
+print(paste("Registros originales:", nrow(data_ocurrencias)))
+print(paste("Registros filtrados:", nrow(data_ocurrencias_filtered)))
+
+glimpse(data_ocurrencias_filtered)
+
+
+# Esto no lo uso mucho porque data=0 --------------------------------------
+#unique(data_ocurrencias$issues)
+#data_ocurrencias_filtered <- data_ocurrencias %>%  #OJO, ESTO ELIMINA TODOS LOS DATOS
+#  filter(issues == "")  # Solo registros sin problemas reportados
+
+
+# sobre instituciones de origen -------------------------------------------
+#unique(data_ocurrencias$publishingOrgKey)
+
+# Función para obtener el nombre de la organización por su publishingOrgKey:
+#get_org_name <- function(org_key) {              
+#  url <- paste0("https://api.gbif.org/v1/organization/", org_key)
+#  response <- fromJSON(content(GET(url), "text"), flatten = TRUE)
+#  return(response$title)
+#}
+
+# Aplicar la función a los códigos únicos en los datos:
+#org_names <- unique(data_ocurrencias$publishingOrgKey)
+#org_info <- sapply(org_names, get_org_name)
+
+# Ver los resultados y analizar si alguno no corresponde:
+#print(org_info)
+
+
+# Fecha (actuales) --------------------------------------------------------
+# esto no porque el modelo actual abarca hartos años:
+#data_ocurrencias_filtered <- data_ocurrencias_filtered %>%  
+#  filter(year >= 2023)  
+
