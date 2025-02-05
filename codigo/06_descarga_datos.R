29770eb4861ea248484e71eaead458446fb41921
Fran <fmnarbona@uc.cl>
2025-02-05 12:37:04 -0300

    se incluye descargar datos

--- codigo/06_descarga_datos.R
+++ codigo/06_descarga_datos.R
@@ -1,9 +1,18 @@
+# Descarga capas climáticas -----------------------------------------------
+writeRaster(capas_present_raster, "capas_clima_1980-2010.tif", overwrite = TRUE)
+writeRaster(capas_future_raster, "capas_clima_1980-2010.tif", overwrite = TRUE)
+
+# Descarga datos de ocurrencia --------------------------------------------
+df_sf <- st_as_sf(data_ocurrencias_filtered, coords = c("decimalLongitude", "decimalLatitude"), crs = 4326)
+st_write(df_sf, "puntos_M-coypus.shp", delete_layer = TRUE)  #cambiar nombre especie si corresponde
 
 # Descarga SDMs de dismo() ------------------------------------------------
 writeRaster(pred_present_resampled, "pred_dismo_1980-2010_N-tarapacana.tif", overwrite = TRUE)  #cambiar nombre especie si corresponde
-writeRaster(pred_future_resampled, "pred_dismo_2035-2065_N-tarapacana.tif", overwrite = TRUE)
+writeRaster(pred_future_resampled, "pred_dismo_2035-2065_N-tarapacana.tif", overwrite = TRUE)  #cambiar nombre especie si corresponde
 
-
+# Descarga mapa diferencia (dismo - arclim) -------------------------------
+writeRaster(diff_present, "dif_1980-2010_N-tarapacana.tif", overwrite = TRUE)  #cambiar nombre especie si corresponde
+writeRaster(diff_future, "dif_2035-2065_N-tarapacana.tif", overwrite = TRUE)  #cambiar nombre especie si corresponde
 
 
 
